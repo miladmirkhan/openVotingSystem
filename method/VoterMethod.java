335cb75a0678aff8deb59380161373e306636e9f
Milad Mirkhan <msherwani92@gmail.com>
2022-10-22 21:19:41 +0300

    add edit voter by national ID

--- method/VoterMethod.java
+++ method/VoterMethod.java
@@ -1,6 +1,7 @@
 package method;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class VoterMethod<Voter> {
 
@@ -21,5 +22,34 @@ public void editVoter(ArrayList<Voter> list,Voter voter,Voter newVoter){
 }
 
 
+public  void editVoter(ArrayList<Voter> list) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the national ID:");
+    String id = input.nextLine();
+    System.out.println("""
+    What do you want to change ?
+      1-Email
+      2-Mobile Number
+      3-password""");
+    int option = input.nextInt();
+    switch (option) {
+        case 1:
+            System.out.println("Enter the new Email:");
+            String newEmail = input.next();
+            
+            list.stream().filter(x -> x.getNatioxnalID().equals(id)).forEach(x -> x.setEmail(newEmail));
+            break;
+        case 2:
+            System.out.println("Enter the new mobile number:");
+            String newMobileNumber = input.next();
+            list.stream().filter(x -> x.getNationalID().equals(id)).forEach(x -> x.setMobileNumber(newMobileNumber));
+            break;
+        case 3:
+            System.out.println("Enter the new password :");
+            String newPassword = input.next();
+            list.stream().filter(x -> x.getNationalID().equals(id)).forEach(x -> x.setPassword(newPassword));
+            break;
+    }
+}
 
 }
