9fc8dcc27230e72c283083990620a3fc297db5d8
milad Mirkhan <msherwani92@gmail.com>
2022-10-22 20:34:11 +0300

    add edit and add voter

--- method/VoterMethod.java
+++ method/VoterMethod.java
@@ -3,10 +3,23 @@ package method;
 import java.util.ArrayList;
 
 public class VoterMethod<Voter> {
-    
+
+// add 
 public void addVoter(ArrayList<Voter> list,Voter voter){
 
     list.add(voter);
 }
+// edit
+public void editVoter(ArrayList<Voter> list,int index,Voter voter){
+    list.set(index, voter);
+}
+
+public void editVoter(ArrayList<Voter> list,Voter voter,Voter newVoter){
+
+    int index = list.indexOf(voter);
+    list.set(index, newVoter);
+}
+
+
 
 }
