05d673bedf6be2fb50a7cac44b6ec8b9a8acc1f4
Milad Mirkhan <msherwani92@gmail.com>
2022-11-05 15:55:00 +0300

    done with voterApplication

--- method/VoterMethod.java
+++ method/VoterMethod.java
@@ -18,6 +18,52 @@ public void addVoter(ArrayList<Voter> list, Voter voter){
 
     list.add(voter);
 }
+public void addVoter(ArrayList<Voter> voterlist){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your full name:");
+    String fullName = input.next();
+    System.out.println("Enter your age:");
+    String age = input.next();
+    System.out.println("Enter your national ID:");
+    String nationalID = input.next();
+    System.out.println("Enter your email:");
+    String email = input.next();
+    System.out.println("Enter your mobile number:");
+    String mobileNumber = input.next();
+    System.out.println("Enter your password:");
+    String password = input.next();
+    System.out.println("Enter your address:");
+    String address = input.next();
+    System.out.println("Enter your zone:");
+    String zone = input.next();
+    
+    Voter voter = new Voter(fullName,age, nationalID, email, mobileNumber, password, address,zone);
+    voterlist.add(voter);
+    input.close();
+    System.out.println("Voter added successfully!");
+
+}
+
+public void addVoteByVoter(ArrayList<Voter> list){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your national ID:");
+    String nationalID = input.next();
+    for (Voter voter : list) {
+        if (voter.getNationalID().equals(nationalID)) {
+            System.out.println("Enter your first ranked vote:");
+            String rankedVote1 = input.next();
+            System.out.println("Enter your second ranked vote:");
+            String rankedVote2 = input.next();
+            System.out.println("Enter your third ranked vote:");
+            String rankedVote3 = input.next();
+            Vote vote = new Vote(true, rankedVote1, rankedVote2, rankedVote3);
+            voter.setVote(vote);
+            System.out.println("Vote added successfully!");
+        }
+    }
+    input.close();
+
+}
 // edit
 public void editVoter(ArrayList<Voter> list,int index,Voter voter){
     list.set(index, voter);
@@ -67,6 +113,23 @@ public ArrayList<String> displayVoter(ArrayList<Voter> voters){
         voterDisplay.add(voter.toString());
     }
     return voterDisplay;
+}
+
+// vote with voter
+public void voteWithVoter(ArrayList<Voter> voters,ArrayList<Vote> votes, Vote vote){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your national ID:");
+    String id = input.nextLine();
+    System.out.println("Enter your password:");
+    String password = input.nextLine();
+    for(Voter voter: voters){
+        if(voter.getNationalID().equals(id) && voter.getPassword().equals(password)){
+            votes.add(vote);
+            System.out.println("Vote added successfully!");
+            break;
+        }
+    }
+    input.close();
 }
 
 public void deleteVoter(ArrayList<Voter> voters, Voter voter){
@@ -180,4 +243,115 @@ public static void readVoteListFromJson(ArrayList<Vote> votes) throws IOExceptio
 
 }
 
+// make a while loop to sign in and sign up and delete and edit and display all voters and display all votes
+public void voterApplication(ArrayList<Voter> voterlist){
+
+    while(true){
+        System.out.println("Welcome to the voter application");
+        System.out.println("1- Sign up");
+        System.out.println("2- Sign in");
+        System.out.println("3- Delete voter");
+        System.out.println("4- Edit voter");
+        System.out.println("5- Display all voters");
+        System.out.println("6- vote");
+        System.out.println("7-send voter list to json");
+        System.out.println("8-get voter list from json");
+        System.out.println("9- Exit");
+        Scanner input = new Scanner(System.in);
+        int option = input.nextInt();
+        switch (option) {
+            case 1:
+            addVoter(voterlist);
+                break;
+            case 2:
+            signInVoter(voterlist);
+                break;
+            case 3:
+                     System.out.println("1- Delete voter by national id");
+                     System.out.println("2- Delete voter by full name");
+                      int deleteOption = input.nextInt();
+                                switch (deleteOption) {
+                                    case 1:
+                                    System.out.println("Enter national id");
+                                    String nationalId = input.next();
+                                    deleteVoterByNationalId(voterlist, nationalId);
+                                        break;
+                                    case 2:
+                                    System.out.println("Enter full name");
+                                    String fullName = input.next();
+                                    deleteVoterByFullName(voterlist, fullName);
+                                        break;
+                                    default:
+                                        break;
+                                }
+                break;
+            case 4:
+            editVoter(voterlist);
+                 
+                break;
+            case 5:
+                displayVoter(voterlist);
+                break;
+            case 6:
+            addVoteByVoter(voterlist);
+                break;
+           
+            case 7:
+            try {
+                sendVoterListToJson(voterlist);
+            } catch (IOException e) {
+                
+                e.printStackTrace();
+            }
+                break;    
+
+            case 8:
+            try {
+                readVoterListFromJson(voterlist);
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+                break;
+            case 9:
+                
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+
+
+            }
+            input.close();
+        }
+        
+    }
+
+
+
+public void signInVoter(ArrayList<Voter> list){
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your national ID:");
+    String id = input.next();
+    System.out.println("Enter your password:");
+    String pass = input.next();
+    if (signIn(list, id, pass)) {
+        System.out.println("You are signed in");
+    } else {
+        System.out.println("You are not signed in");
+    }
+    input.close();
+}
+
+
+private boolean signIn(ArrayList<Voter> list, String id, String pass) {
+    for (int i = 0; i < list.size(); i++) {
+        if (list.get(i).getNationalID().equals(id) && list.get(i).getPassword().equals(pass)) {
+            return true;
+        }
+    }
+    return false;
+}
 }
