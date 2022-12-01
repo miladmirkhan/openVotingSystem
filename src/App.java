3d79000f080ddb64351c695c5260df547b5dd157
Rahen Mohammed <rahenmqadaragha@gmail.com>
2022-12-01 19:00:25 +0300

    refactor the whole code

---
+++ src/App.java
@@ -0,0 +1,9 @@
+
+import views.MainTerminal;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        MainTerminal.loginTerminal();
+//
+    }
+}
---
+++ src/connections/JDBC.java
@@ -0,0 +1,69 @@
+package connections;
+import java.sql.*;
+
+
+public class JDBC {
+
+    public static void insert(String sql){
+                try {
+                    // Class.forName("com.mysql.cj.jdbc.Driver");
+                    // Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/openvotingsystem","root",""); 
+                    Class.forName(JDBCDRIVER.getClassName().className);
+                    Connection connection = DriverManager.getConnection(JDBCLINK.getUrl().finalUrl,"root",""); 
+                    Statement statement = connection.createStatement();
+                    statement.executeUpdate(sql);
+                    
+                } catch (ClassNotFoundException e) {
+        
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+        }
+    }
+    public static ResultSet display(String sql) {
+        ResultSet resultSet = null;
+        try {
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+            // Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/openvotingsystem","root","");
+            Class.forName(JDBCDRIVER.getClassName().className);
+            Connection connection = DriverManager.getConnection(JDBCLINK.getUrl().finalUrl,"root",""); 
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);//read only
+        } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public static void remove(String sql) throws SQLException {
+        try {
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+            // Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/openvotingsystem","root","");
+            Class.forName(JDBCDRIVER.getClassName().className);
+            Connection connection = DriverManager.getConnection(JDBCLINK.getUrl().finalUrl,"root",""); 
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+        }
+    }   
+    
+    public static void update(String sql) throws SQLException {
+        try {
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+            // Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/openvotingsystem","root","");
+            Class.forName(JDBCDRIVER.getClassName().className);
+            Connection connection = DriverManager.getConnection(JDBCLINK.getUrl().finalUrl,"root",""); 
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+        }
+    }    
+
+}
